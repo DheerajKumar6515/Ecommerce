b4fcd7418312d614ce299577954d2492a338130f
Dheeraj <kumardheeraj6515@gmail.com>
2026-03-24 03:56:02 +0530

    change url

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-
+const DB_URL1=https://ecommerce-frontend-7p13.onrender.com;
 const connectTodb = async()=>{
     try {
-        await mongoose.connect(`${process.env.DB_URL}`).then(()=>{
+        await mongoose.connect(`${process.env.DB_URL1}`).then(()=>{
             console.log('connection successfully👍');          
         })
     } catch (e) {
